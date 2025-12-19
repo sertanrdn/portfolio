f0bbc4bd43628c1f16963ac157e7a01136b185cc
sertanrdn <sertanerdogan07@gmail.com>
2025-12-19 19:52:02 +0100

    Created custom hook to highlight active section for scroll-spy and used in navbar component

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import Link from "next/link";
+import { useActiveSection } from "../hooks/useActiveSection";
 
 const navItems = [
     { label: "About", href: "#about" },
@@ -10,16 +11,24 @@ const navItems = [
 ];
 
 export default function Navbar() {
+    const sectionId = navItems.map(i => i.href.slice(1));
+    const activeId = useActiveSection(sectionId);
+
     return (
         <header className="fixed top-0 w-full border-b border-zinc-200 bg-zinc-50/80 backdrop-blur dark:border-zinc-800 dark:bg-black/80">
             <nav className="mx-auto flex h-16 max-w-6xl items-center justify-between px-4">
                 <span className="font-bold text-xl">Sertan Erdogan</span>
-                <ul className="item-center gap-6 flex">
+                {/* Desktop Nav */}
+                <ul className="items-center gap-6 flex">
                     {navItems.map((item) => (
                         <li key={item.href}>
                             <Link
                                 href={item.href}
-                                className="text-sm text-zinc-600 transition hover:text-zinc-900 dark:text-zinc-400 dark:hover:text-zinc-100"
+                                className={`text-sm transition ${
+                                    activeId === item.href.slice(1)
+                                    ? "text-primary dark:text-primary-400"
+                                    : "text-zinc-600  hover:text-zinc-900 dark:text-zinc-400 dark:hover:text-zinc-100"
+                                    }`}
                             >
                                 {item.label}
                             </Link>

0a62375ade71d0e2f552c5c775bf82f75dc9bdcb
sertanrdn <sertanerdogan07@gmail.com>
2026-01-19 15:15:02 +0100

    Created Footer component to use on the page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import Navbar from "../components/Navbar";
 import Projects from "../components/Projects";
 import Skills from "../components/Skills";
 import Contact from "../components/Contact";
+import Footer from "../components/Footer";
 
 export default function Home() {
   return (
@@ -21,6 +22,8 @@ export default function Home() {
         <Skills />
         {/* Contact */}
         <Contact />
+        {/* Footer */}
+        <Footer />
       </main>
     </>
   );

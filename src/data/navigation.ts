cf647852127d4a7e2399f52882c21a01a2232f90
sertanrdn <sertanerdogan07@gmail.com>
2026-01-19 14:19:38 +0100

    Separated navItem data to use later in the other components

---
+++ src/data/navigation.ts
@@ -0,0 +1,6 @@
+export const navItems = [
+    { label: "About", href: "#about" },
+    { label: "Projects", href: "#projects" },
+    { label: "Skills", href: "#skills" },
+    { label: "Contact", href: "#contact" }
+];
